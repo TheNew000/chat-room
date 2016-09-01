2a7724ae3c2b4ff3cf70255a673056d9bb0a762e
TheNew000 <mahkatorocks@gmail.com>
2016-09-01 00:24:05 -0400

    Added Erase Feature

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ var socketIo = require('socket.io');
 // listen to the server which is listening on port XXXX
 var io = socketIo.listen(server);
 var socketUsers = [];
+var chatHistory = [];
+var currentCanvas = [];
 //We need to deal wiht a new socket connection
 io.sockets.on('connect', function(socket){
 	// socketUsers.push(socket);
@@ -67,6 +69,13 @@ io.sockets.on('connect', function(socket){
 			date: data.date
 		});
 	});
+
+	socket.on('drawing_to_server', function(drawingData){
+		if(drawingData.lastMousePosition !== null){
+			io.sockets.emit('drawing_to_client', drawingData);
+		}
+	});
+
 	socket.on('disconnect', function(){
 		console.log(socket.id + " -- user has disconnected");
 		for (var i = 0; i < socketUsers.length; i++) {
